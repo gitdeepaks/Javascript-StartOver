d1ee7423e3014eff7a6dd686d2d6ff42585b07fd
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-03 17:39:15 +0530

    operators

--- Fundamentals-01/Hello.js
+++ Fundamentals-01/Hello.js
@@ -13,20 +13,42 @@
 // console.log(firstName);
 
 //variables let, var, const
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
 
-const birthyear = 1991;
-// birthyear = 1992 // not possible immutable varible
+// const birthyear = 1991;
+// // birthyear = 1992 // not possible immutable varible
 
-// const job; //not possible
+// // const job; //not possible
 
 
 
-var job = 'programmer'
-job = 'teacher'
+// var job = 'programmer'
+// job = 'teacher'
 
 
 lastname = 'Sankhyan'
 console.log(lastname);
+
+
+//Opertators
+
+const now = 2037
+const ageDeepak = now - 1991
+const ageRitika = now - 1994
+console.log(ageDeepak, ageRitika);
+console.log(ageDeepak * 2, ageDeepak / 2, ageRitika ** 2);
+
+const forstname = 'Deepak';
+const lastNam = 'Sankhyan';
+
+console.log(forstname + lastNam);
+
+console.log(forstname + ' ' + lastNam);
+
+// assignment operator
+
+let x = 10 + 5;
+console.log(x);
+
