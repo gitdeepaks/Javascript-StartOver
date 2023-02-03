ef92852efc54b3227ad46fa60087656edeb8124c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-03 17:02:28 +0530

    let

--- Fundamentals-01/Hello.js
+++ Fundamentals-01/Hello.js
@@ -1,13 +1,30 @@
-let js = 'amazing';
+// let js = 'amazing';
 
-if (js === 'amazing') {
-    alert("JavaScript is fun")
-}
+// if (js === 'amazing') {
+//     alert("JavaScript is fun")
+// }
 
 
-console.log(40 + 8 + 23 - 10);
-//let, var, const
+// console.log(40 + 8 + 23 - 10);
+// //let, var, const
 
-let firstName = "Jonas"
+// let firstName = "Jonas"
 
-console.log(firstName);
+// console.log(firstName);
+
+//variables let, var, const
+let age = 30;
+age = 31;
+
+
+const birthyear = 1991;
+// birthyear = 1992 // not possible immutable varible
+
+// const job; //not possible
+
+
+
+var job = 'programmer'
+job = 'teacher'
+
+
