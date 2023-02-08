a48024d10a6724dac74d72051473d352ea80d7da
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-08 10:55:18 +0530

    coding challange 3

---
+++ Fundamentals-01/CodingChallange3.js
@@ -0,0 +1,31 @@
+// const scoreDolphines = (96 + 108 + 89) / 3;
+
+// const scoreKoalas = (88 + 91 + 110) / 3
+
+
+// console.log(scoreDolphines, scoreKoalas);
+
+// if (scoreDolphines > scoreKoalas) {
+//     console.log('Dolphinines Wins');
+// } else if (scoreKoalas > scoreDolphines) {
+//     console.log('koalas wins');
+// } else if (scoreDolphines === scoreKoalas) {
+//     console.log('Both wins');
+// }
+
+//Bonus
+
+const scoreDolphines = (97 + 112 + 101) / 3;
+
+const scoreKoalas = (109 + 95 + 123) / 3
+
+
+console.log(scoreDolphines, scoreKoalas);
+
+if (scoreDolphines > scoreKoalas) {
+    console.log('Dolphinines Wins');
+} else if (scoreKoalas > scoreDolphines) {
+    console.log('koalas wins');
+} else if (scoreDolphines === scoreKoalas) {
+    console.log('Both wins');
+}
