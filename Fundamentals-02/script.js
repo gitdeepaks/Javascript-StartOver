ee5b1770b2cf4635f3844d370551e09b79a164cf
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-09 17:33:28 +0530

    strict mode

---
+++ Fundamentals-02/script.js
@@ -0,0 +1,15 @@
+//Strict mode
+
+'use strict';
+
+let hasDriverLicence = false;
+const passTest = true;
+
+
+if (passTest) hasDriverLicence = true;
+
+if (hasDriverLicence) console.log('I can Drive :D');
+
+// const interface = 'Audio' 
+
+// const private = 534;
