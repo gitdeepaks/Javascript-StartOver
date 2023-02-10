d19176e6953d54c4a02d96de2f1ad75bac061810
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 20:30:02 +0530

    oparatots

---
+++ Fundamentals-02/01-variables-data-types/07-operators/script.js
@@ -0,0 +1,54 @@
+//Arithematic
+
+let x;
+x = 5 + 5
+x = 5 + 5
+x = 5 - 5
+x = 5 * 5
+x = 5 / 5
+x = 5 % 5
+
+//Concatination
+
+x = 'Hello' + 'World'
+
+//Exponenet
+x = 2 ** 3
+
+//increment
+
+x = 1
+// x = x + 1
+
+x++
+
+//decrement
+
+// x = x - 1
+
+x--
+
+//Assignment
+
+x = 10
+
+x += 5
+x -= 5
+x *= 5
+x /= 5
+x %= 5
+x **= 5
+
+//comparison
+
+x = 2 == '2'
+x = 2 === '2'
+x = 2 !== '2'
+
+x = 10 > 5
+
+x = 10 < 5
+x = 10 > 5
+x = 10 >= 5
+x = 10 <= 5
+console.log(x)
