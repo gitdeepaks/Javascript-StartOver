b06a0f4fcbbeaa94f68d48c2539b8ae73be64a4e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 19:14:09 +0530

    datatypes

---
+++ Fundamentals-02/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,42 @@
+const firtName = 'Sara';
+
+const output = firtName
+
+
+//Number
+
+const age = 30
+const temp = 98.9;
+
+//Boolean 
+const hasKisd = true
+
+//Null
+
+
+const aptNumber = null;
+
+//undefined
+// let score = output;
+const score = undefined
+
+//Symbol
+
+const id = Symbol('id')
+// const output = aptNumber;
+
+// const output = Number;
+
+
+//objects
+
+const person = {
+    name: 'John',
+};
+
+function sayHello() {
+    console.log('Hello')
+}
+console.log(output, typeof output)
+
+
