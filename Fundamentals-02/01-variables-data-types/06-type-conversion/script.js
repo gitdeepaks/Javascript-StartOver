5a639bbabd3bbc405e8b36ec519386693ae7b1ad
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 20:18:20 +0530

    type conversion

---
+++ Fundamentals-02/01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,31 @@
+let amount = 'hello';
+
+//change string to number
+amount = parseInt(amount)
+
+// amount = +amount
+
+// amount = Number(amount)
+
+
+//change a number to string
+
+// amount = amount.toString();
+
+// amount = String(amount)
+
+//chnage string to decimal
+
+// amount = parseFloat(amount)
+
+
+//convert number to boolean
+
+// amount = Boolean(amount);
+
+console.log(Math.sqrt(-1));
+
+console.log(1 + NaN)
+console.log(undefined + undefined)
+console.log('foo' / 3)
+console.log(amount, typeof amount)
