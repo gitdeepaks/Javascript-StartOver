8d7f372ce639d41cf1e07b867439ec9b67f77ade
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 20:36:43 +0530

    type coercion

---
+++ Fundamentals-02/01-variables-data-types/09-strings/script.js
@@ -0,0 +1,31 @@
+//Strict mode
+
+// 'use strict';
+
+// let hasDriverLicence = false;
+// const passTest = true;
+
+
+// if (passTest) hasDriverLicence = true;
+
+// if (hasDriverLicence) console.log('I can Drive :D');
+
+// // const interface = 'Audio' 
+
+// // const private = 534;
+//this is single line pof code 
+
+
+// console.log(100);
+console.log('Hello world');
+
+console.error('Alert');
+console.warn('Warning');
+
+console.table({ name: 'Deepak', email: 'deepak@gmail.com' })
+
+// conslole.group('Simple')
+
+const styles = 'Padding: 10px; background-color: white; color:green'
+
+console.log('%cHello ', styles);
