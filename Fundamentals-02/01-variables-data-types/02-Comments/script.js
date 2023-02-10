b02a208acc3d22d7ebc0ef5399e0dd612c7093c2
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 18:58:11 +0530

    console

--- Fundamentals-02/01-variables-data-types/02-Comments/script.js
+++ Fundamentals-02/01-variables-data-types/02-Comments/script.js
@@ -13,8 +13,10 @@
 // // const interface = 'Audio' 
 
 // // const private = 534;
+//this is single line pof code 
 
-console.log(100);
+
+// console.log(100);
 console.log('Hello world');
 
 console.error('Alert');
---
+++ Fundamentals-02/01-variables-data-types/03-Variables/script.js
@@ -0,0 +1,68 @@
+// ways to declare a variable
+//var let const
+
+
+let firstName = 'John';
+
+let lastName = 'Dow'
+
+
+console.log(firstName, lastName)
+
+//Naming Conventions
+
+// let 1name = 'brad'
+
+
+//Re-assigning variables
+
+age = 31;
+
+
+console.log(age)
+
+
+let score = 1;
+
+
+console.log(score)
+
+if (true) {
+    score = score + 1;
+
+
+}
+
+//Declare Multile values
+
+let a, b, c;
+
+
+const d = 10, e = 10, f = 11;
+
+console.log(d)
+console.log(a)
+
+const x = 100;
+
+// const score1;
+
+const arr = [1, 2, 3, 4]
+
+
+arr.push(5);
+
+
+console.log(arr)
+
+
+const person = {
+    name: 'Deepak',
+};
+
+person.name = 'John';
+
+person.email = 'john@example.com';
+
+
+console.log(person)
