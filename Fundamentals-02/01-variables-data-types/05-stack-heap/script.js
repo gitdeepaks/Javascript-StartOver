5d6c52d7afd59c9da0eb6febf44d07a0d46bae56
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-10 20:06:40 +0530

    stack-heap

---
+++ Fundamentals-02/01-variables-data-types/05-stack-heap/script.js
@@ -0,0 +1,27 @@
+
+//Stored in Stack
+const name = 'John'
+
+
+const age = 30;
+
+//stored in heap
+
+const person = {
+    name: 'Deepak',
+    age: 30,
+};
+
+
+let newName = name;
+
+
+newName = 'Divya';
+
+let newPerson = person;
+
+
+console.log(name, newName);
+console.log(person, newPerson);
+
+
