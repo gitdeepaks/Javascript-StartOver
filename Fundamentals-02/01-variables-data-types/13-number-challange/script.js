93b21df0ddde645aefb5311a98d08cf94d98c6ee
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 13:32:45 +0530

    math objects

---
+++ Fundamentals-02/01-variables-data-types/13-number-challange/script.js
@@ -0,0 +1,24 @@
+const x = Math.random() * 100 + 1;
+const y = Math.random() * 50 + 1;
+
+//Get the sum
+const sum = x + y;
+const sumTotal = `${x} + ${y} = ${sum}`
+console.log(sumTotal)
+
+
+//get the difference
+const difference = x - y
+const getDifference = `${x} - ${y} = ${difference}`
+console.log(getDifference)
+
+//get the product
+const prod = x * y
+const getProd = `${x} x ${y} = ${prod}`
+console.log(getProd)
+
+//get the quotient
+const quot = x / y
+const getQuot = `${x} / ${y} = ${quot}`
+console.log(getQuot)
+
