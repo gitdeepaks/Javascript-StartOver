60f07a71b4076eaaae4fc12ee7c48f390f628028
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 19:34:29 +0530

    arrays mehtods

---
+++ 02-arrays-and-objects/02arrays-methods/script.js
@@ -0,0 +1,25 @@
+//special types of objeststhat store multiple values
+
+const numbers = [44, 55, 66, 77, 67, 45]
+
+//Arrays constructors
+
+const fruit = new Array('a', 'b', 'c', 'd', 'e', 'f')
+console.log(numbers)
+
+let x;
+
+x = numbers[0];
+x = numbers[0] + numbers[3];
+
+
+x = `My favorite fruit is ${fruit[4]}`
+
+x = numbers.length;
+
+
+fruit[3] = 'droople';
+x = fruit
+
+x = fruit.length
+console.log(x)
