304580c3b656f81e9f86237e407b20f8e4a43ea5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 12:38:54 +0530

    math objects

---
+++ Fundamentals-02/01-variables-data-types/12-math-objects/script.js
@@ -0,0 +1,16 @@
+let x;
+
+
+x = Math.sqrt(9)
+x = Math.abs(9)
+x = Math.round(4.3)
+x = Math.ceil(4.2)
+x = Math.floor(4.9)
+x = Math.pow(2, 9)
+
+x = Math.min(4, 7, 8)
+x = Math.max(4, 7, 8)
+x = Math.random()
+
+x = Math.floor(Math.random() * 10 + 1);
+console.log(x);
