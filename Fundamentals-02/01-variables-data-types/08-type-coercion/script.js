2a09ee3be2d4a1d3f1a0abd174456e10c7457629
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 11:54:21 +0530

    update repo

---
+++ Fundamentals-02/01-variables-data-types/08-type-coercion/script.js
@@ -0,0 +1,23 @@
+let x;
+
+x = 5 + '5';
+x = 5 + Number('5');
+
+
+x = 5 * '5';
+
+x = 5 + null;
+
+x = Number(null)
+
+x = Number(true)
+x = Number(false)
+
+
+x = 5 + true;
+
+x = 5 + false;
+
+x = x + undefined
+
+console.log(x, typeof x)
