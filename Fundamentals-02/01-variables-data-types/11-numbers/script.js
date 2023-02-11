aab03cd67bd6ee16a49d92756fa80dbdee71fb34
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 12:29:10 +0530

    numbers

--- Fundamentals-02/01-variables-data-types/11-numbers/script.js
+++ Fundamentals-02/01-variables-data-types/11-numbers/script.js
@@ -1,1 +1,22 @@
-clg('hello')
+let x;
+
+
+const num = new Number(5);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(3);
+x = num.toExponential(2);
+
+x = num.toLocaleString('en-US');
+
+x = num.valueOf()
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+console.log(num);
