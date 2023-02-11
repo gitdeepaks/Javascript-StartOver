f772408a6995607c3b0e1cfcb341377b11db9e28
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-11 14:33:56 +0530

    date&time

---
+++ Fundamentals-02/01-variables-data-types/14-dates-and-time/script.js
@@ -0,0 +1,20 @@
+let d;
+
+d = new Date()
+
+d = d.toString()
+d = new Date(2023, 2, 11, 14, 27, 00)
+d = new Date('2023-2-11')
+d = new Date('7/10/2022 12:30:00')
+d = new Date('7/10/2022 12:30:00')
+d = new Date('2022-10-10')
+
+d = Date.now()
+d = new Date('07-10-2022')
+d = d.getTime()
+
+d = d.valueOf()
+
+d = Math.floor(Date.now() / 1000)
+
+console.log(d)
