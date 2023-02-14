690c12bde884dd1a9016783ed0fd2e2dfa9fd4c6
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-14 22:11:39 +0530

    some complecated topics

---
+++ 03-functions-scope-excution-context/11-call-stack/script.js
@@ -0,0 +1,13 @@
+function first() {
+    console.log('first...');
+}
+function second() {
+    console.log('second...');
+}
+function third() {
+    console.log('third...');
+}
+
+first();
+second();
+third();
