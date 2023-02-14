691367fdcb1106508f2ffdfc9005f30a97b5c42e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-14 16:43:15 +0530

    nested scope

---
+++ 03-functions-scope-excution-context/05-nested-scope/script.js
@@ -0,0 +1,23 @@
+
+function first() {
+    const x = 100;
+
+    function second() {
+        const y = 200;
+        console.log(x + y)
+    }
+
+    second();
+}
+
+first();
+
+if (true) {
+    const x = 100;
+
+    if (x === 100) {
+        const y = 200;
+        console.log(x + y)
+    }
+    console.log(y)
+}
