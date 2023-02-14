def11edf0d2bdf2b2001d3013a66577268c2f2a0
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-14 22:24:56 +0530

    logical statements

---
+++ 04-logic-and-controlflow/01-if-statements/script.js
@@ -0,0 +1,32 @@
+if (true) {
+    console.log("this is true")
+}
+
+if (false) {
+    console.log("this is NOT true")
+}
+
+const x = 10;
+const y = 4;
+
+if (x >= y) {
+    console.log(`${x} is grreater that ${y}`);
+}
+if (x === y) {
+    console.log(`${x} is equal to ${y}`);
+} else {
+    console.log(`${x} is not eqaual to ${y}`)
+}
+
+if (x !== y) {
+    const z = 20;
+    console.log(`${z} is 20`)
+}
+
+//shorthand
+
+if (x >= y) console.log(`${x} is grreater that ${y}`),
+    console.log("that is true")
+else console.log('this is false')
+
+
