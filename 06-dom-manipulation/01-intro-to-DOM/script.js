3871d4458bea907704b376ff7ec71835224da09f
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-16 19:45:09 +0530

    DOM

---
+++ 06-dom-manipulation/01-intro-to-DOM/script.js
@@ -0,0 +1,19 @@
+// // console.log(window)
+// console.log(window.document)
+
+
+// console.log(document.body.innerText)
+console.log(document.links[0])
+
+// document.body.innerHTML = '<h1> Hello! </h1>'
+
+
+// document.write('Hello from JS')
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main')
+
+// main.innerHTML = '<h1> Hello from JS main </h1>'
+
+document.querySelector('#main h1').innerText = 'Hello queryselector';
+
