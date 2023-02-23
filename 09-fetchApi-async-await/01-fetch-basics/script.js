4cb93d9b55a2c5d4693b72f1e564e78d85e2c467
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-23 22:05:38 +0530

    fetch api

---
+++ 09-fetchApi-async-await/01-fetch-basics/script.js
@@ -0,0 +1,27 @@
+//Fetching from JSOM file
+
+window.fetch('./movies.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+
+//Fetching from text file
+window.fetch('./test.txt')
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+//Fetching from an API
+
+window.fetch('https://api.github.com/users/gitdeepaks')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        document.querySelector('h1').textContent = data.login
+    });
