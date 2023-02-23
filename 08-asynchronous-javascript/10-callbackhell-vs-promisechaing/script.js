5a19231ed2bb17e2af1a496dfdd0349b414773a7
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-23 16:24:49 +0530

    promiss vs callbak hell

---
+++ 08-asynchronous-javascript/10-callbackhell-vs-promisechaing/script.js
@@ -0,0 +1,37 @@
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', endpoint);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject('SOmething went wrong')
+        }
+      }
+    };
+
+    setTimeout(() => {
+      xhr.send();
+
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
+
+}
+
+getData('./movies.json').then((movies) => {
+  console.log(movies);
+  return getData('./actors.json')
+})
+  .then((actors) => {
+    console.log(actors)
+    return getData('./directors.json');
+  })
+  .then((directors) => { console.log(directors) })
+  .catch((err) => { console.log(err) });
+
