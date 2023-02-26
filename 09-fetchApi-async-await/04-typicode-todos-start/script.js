03c9622fa574dcc4eafe0e3087e532b4be7b0fee
gitdeepaks <deepaksankhyan92@outlook.com>
2023-02-26 16:34:35 +0530

    mini project

--- 09-fetchApi-async-await/04-typicode-todos-start/script.js
+++ 09-fetchApi-async-await/04-typicode-todos-start/script.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-    fetch(apiUrl + '?_limit=5')
+    fetch(apiUrl + '?_limit=10')
         .then((response) => response.json())
         .then((data) => {
             data.forEach((todo) => addTodotoDOm(todo)
@@ -12,6 +12,7 @@ const getTodos = () => {
 
 const addTodotoDOm = (todo) => {
     const div = document.createElement('div');
+    div.classList.add('todo');
     div.appendChild(document.createTextNode(todo.title));
     div.setAttribute('data-id', todo.id)
     if (todo.completed) {
@@ -37,9 +38,51 @@ const createTodos = (e) => {
         .then(data => addTodotoDOm(data))
 };
 
+const toggleComplete = (e) => {
+    if (e.target.classList.contains('todo')) {
+
+        e.target.classList.toggle('done');
+
+        updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+
+
+    }
+};
+
+
+function updateTodo(id, completed) {
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ completed }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+
+    }
+    );
+}
+
+function deleteTodo(e) {
+    if (e.target.classList.contains('todo')) {
+
+        const id = e.target.dataset.id;
+        fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                res.json()
+            })
+            .then(() => e.target.remove())
+
+
+    }
+}
+
 const init = () => {
     document.addEventListener('DOMContentLoaded', getTodos);
     document.querySelector('#todo-form').addEventListener('submit', createTodos);
+    document.querySelector('#todo-list').addEventListener('click', toggleComplete);
+    document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 
 }
 
