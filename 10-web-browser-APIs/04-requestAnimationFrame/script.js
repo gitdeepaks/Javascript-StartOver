355b88380f786c3cb3e9f8de41d621abec096658
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-05 18:03:22 +0530

    canvas animation

---
+++ 10-web-browser-APIs/04-requestAnimationFrame/script.js
@@ -0,0 +1,52 @@
+const image = document.querySelector('img');
+let start;
+let done = false;
+
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+
+  const elapsed = timestamp - start;
+
+  if (elapsed > 5000) {
+    done = true;
+  }
+
+  if (done) {
+    return;
+  }
+
+  image.style.transform = `translateX(${elapsed / 10}px) rotate(${elapsed / 10
+    }deg)`;
+
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
+// const image = document.querySelector('img');
+// let start;
+
+// let done = false;
+
+
+// function step(timestamp) {
+//   if (start === undefined) {
+//     start = timestamp;
+//   }
+
+//   const elapsed = timestamp - start;
+
+//   if (elapsed > 5000) {
+//     done = true;
+//   }
+
+//   if (done) {
+//     return;
+//   }
+
+//   image.style.transform = `translateX(${elapsed / 10}px) rotate(${elapsed / 10
+//     }deg)`;
+
+//   requestAnimationFrame(step);
+// }
