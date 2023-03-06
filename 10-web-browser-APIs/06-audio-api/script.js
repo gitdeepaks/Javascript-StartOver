d5cf8e523d9d0bb8762b7b045aaa6e09c56766d5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-06 22:35:30 +0530

    audio

---
+++ 10-web-browser-APIs/06-audio-api/script.js
@@ -0,0 +1,6 @@
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const stop = document.getElementById('stop');
+const currentTime = document.getElementById('current-time');
+const volume = document.getElementById('volume');
