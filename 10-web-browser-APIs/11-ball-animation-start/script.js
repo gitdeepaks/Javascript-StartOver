2eb0b47f7b899f5a9e87449ca65f66aa5cd9da57
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-08 16:00:42 +0530

    animation

---
+++ 10-web-browser-APIs/11-ball-animation-start/script.js
@@ -0,0 +1,38 @@
+const ball = document.getElementById('ball');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const reverse = document.getElementById('reverse');
+const speedUp = document.getElementById('speed-up');
+const speedDown = document.getElementById('slow-down');
+
+const rollAnimation = [{
+    transform: 'rotate(0) translate3D(- 50 %, -50 %, 0)',
+    color: 'white'
+},
+{
+    color: 'blue',
+    offset: 0.3
+},
+{
+    transform: 'rotate(360deg) translate3D(-50%, -50%, 0)',
+    color: 'white'
+}
+
+];
+const rollOptions = {
+    duration: 3000,
+    iterations: Infinity
+}
+
+const roll = ball.animate(rollAnimation, rollOptions)
+
+pause.addEventListener('click', () => roll.pause());
+play.addEventListener('click', () => {
+
+    roll.updatePlaybackRate = 1;
+    roll.play()
+}
+);
+reverse.addEventListener('click', () => roll.reverse());
+speedUp.addEventListener('click', () => roll.playbackRate = roll.playbackRate * 2);
+speedDown.addEventListener('click', () => roll.playbackRate = roll.playbackRate / 2);
