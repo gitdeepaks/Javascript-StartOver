2c1c5a474d917db146a88e3f64978a320844220b
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-08 13:11:40 +0530

    drum project

---
+++ 10-web-browser-APIs/08-drum-machine-start/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('keydown', function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) {
+        return;
+    }
+
+    audio.currentTime = 0
+    audio.play();
+    key.classList.add('playing');
+    setTimeout(() => {
+        key.classList.remove('playing');
+    })
+})
