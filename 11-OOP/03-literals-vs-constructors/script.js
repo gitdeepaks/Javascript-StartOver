1edca794e2a3654479ffafd8ba20cf31957304f9
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-11 19:24:02 +0530

    new commit

--- 11-OOP/03-literals-vs-constructors/script.js
+++ 11-OOP/03-literals-vs-constructors/script.js
@@ -14,4 +14,41 @@ console.log(strLi[0]);
 
 
 //Unboxing
-strObj.valueOf()
+strObj.valueOf(strObj.valueOf(), typeof strObj.valueOf())
+
+console.log(strLi.constructor);
+console.log(strObj.constructor);
+
+
+console.log(strLi instanceof String);
+console.log(strObj instanceof String);
+
+
+// Other types
+const numLit = 20;
+const numObj = new Number(20);
+
+console.log(numLit, typeof numLit);
+console.log(numObj, typeof numObj);
+
+const boolLit = true;
+const boolObj = new Boolean(true);
+
+console.log(boolLit, typeof boolLit);
+console.log(boolObj, typeof boolObj);
+
+const arrLit = [1, 2, 3, 4, 5];
+const arrObj = new Array(1, 2, 3, 4, 5);
+
+console.log(arrLit, typeof arrLit);
+console.log(arrObj, typeof arrObj);
+
+const funcLit = function (x) {
+  return x * x;
+};
+
+console.log(funcLit, typeof funcLit);
+
+console.log(funcLit(5));
+
+const funcObj = new Function('x', 'return x * x');
