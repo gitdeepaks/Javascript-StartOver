8ff499bcf01541fe0f7b8c743b29c478eedaa23a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-13 15:22:31 +0530

    new commit

--- 11-OOP/05-prototypes/script.js
+++ 11-OOP/05-prototypes/script.js
@@ -2,14 +2,38 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-    return this.width * this.height;
-  };
+  // this.area = function () {
+  //   return this.width * this.height;
+  // };
 }
+
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
+
+Rectangle.prototype.parimeter = function () {
+  return 2 * (this.width * this.height);
+};
+
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+};
+
+Rectangle.prototype.changename = function (newName) {
+  return (this.name = newName);
+};
+
 
 const rect = new Rectangle('Rect', 10, 10);
 
 console.log(rect);
+console.log(rect.area())
+console.log(rect.area)
+console.log(rect.parimeter())
+console.log(rect.isSquare())
+console.log(rect.name)
+
+console.log(rect.area())
 // console.log(Object.getPrototypeOf(rect))
 
 
