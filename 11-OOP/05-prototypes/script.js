a97dd9f27ed7b7e03f3518187c22f782ba50dad8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-03-13 15:16:03 +0530

    new commit

---
+++ 11-OOP/05-prototypes/script.js
@@ -0,0 +1,24 @@
+function Rectangle(name, width, height) {
+  this.name = name;
+  this.width = width;
+  this.height = height;
+  this.area = function () {
+    return this.width * this.height;
+  };
+}
+
+const rect = new Rectangle('Rect', 10, 10);
+
+console.log(rect);
+// console.log(Object.getPrototypeOf(rect))
+
+
+
+// //  Rectangle.prototype inherits from Object.prototype. That's why we can use toString(), etc
+// console.log(rect.toString());
+
+// // To get the prototype of an object
+// console.log(Object.getPrototypeOf(rect));
+
+
+
