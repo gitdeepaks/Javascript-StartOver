71ad80ed4082a9c174ebfbc8869be52743de227e
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-05 16:40:35 +0530

    upderstand map reduce

---
+++ 02-arrays-and-objects/04-array-challange/nine.js
@@ -0,0 +1,19 @@
+const muuNums = [1, 2, 3];
+
+// const total = muuNums.reduce(function (acc, curr) {
+//   console.log(`acc: ${acc}, curr: ${curr}`);
+//   return acc + curr;
+// }, 0);
+
+const total = muuNums.reduce((acc, curr) => acc + curr, 0);
+console.log(total);
+
+const shoppingCart = [
+  { itemName: "js Course", price: 299 },
+  { itemName: "mobileDev", price: 399 },
+  { itemName: "data sice", price: 599 },
+];
+
+const price = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(price);
