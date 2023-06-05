4bc5a6851e0893c96f9c02441b36eda0b4d6dac9
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-05 17:03:17 +0530

    upderstand map reduce

--- 02-arrays-and-objects/04-array-challange/nine.js
+++ 02-arrays-and-objects/04-array-challange/nine.js
@@ -17,3 +17,14 @@ const shoppingCart = [
 const price = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
 console.log(price);
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
