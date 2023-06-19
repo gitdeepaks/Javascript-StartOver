965a7339c2439ec3edd58284a49ae27e47ed94b8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-19 20:31:54 +0530

    Commit message

--- 15-Node-core-module/http.js
+++ 15-Node-core-module/http.js
@@ -1,11 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === "/") {
-    res.writeHead(200, { "content-type": "text/plaintext" });
-    res.end("<h1>Welcome!</h1>");
+    fs.readFile("index.html", (err, file) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "text/html" });
+        res.end("<h1>Welcome</h1>");
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(file);
+      }
+    });
+  } else if (url === "/about/posts") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end("<h1>About</h1>");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>Page not found</h1>");
   }
 });
 
