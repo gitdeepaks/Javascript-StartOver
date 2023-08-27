f1f188eb17a3e075d17029adc55e0f87f8f70f8b
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-27 17:53:35 +0530

    Commit message

--- 16-Projests-&-Practice/03-Intermediate-stuff/app.js
+++ 16-Projests-&-Practice/03-Intermediate-stuff/app.js
@@ -48,3 +48,63 @@ console.log(number[1] * 2);
 number.forEach((elem) => {
   console.log(elem * 2);
 });
+
+// objects of array
+
+const data = [
+  {
+    name: "John",
+    age: 23,
+  },
+
+  {
+    name: "Mike",
+    age: 25,
+  },
+  {
+    name: "Ron",
+    age: 28,
+  },
+];
+
+console.log(data[0].age);
+
+// Objects
+
+const person = {
+  name: "John",
+  age: 23,
+  hobbies: ["Sports", "Cooking"],
+  address: {
+    city: "New York",
+    state: "NY",
+  },
+};
+
+console.log(person.name.city);
+
+const username = input.value;
+const password = input.value;
+
+const newUser = {
+  username: username,
+  password: password,
+};
+
+// pasing arguments of function
+
+function logUser(user) {
+  console.log(user.name);
+  console.log(user.age);
+}
+
+function user(params) {
+  name: "Emily";
+  age: 23;
+}
+
+logUser(user);
+
+// increment(++) decrement(--)
+
+let num = 100;
