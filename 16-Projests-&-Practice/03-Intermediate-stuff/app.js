898a75121e797d31950a8af272bcb8088c91b8f5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-27 16:19:14 +0530

    Commit message

--- 16-Projests-&-Practice/03-Intermediate-stuff/app.js
+++ 16-Projests-&-Practice/03-Intermediate-stuff/app.js
@@ -23,3 +23,28 @@ console.log(text.substring(0, 5));
 // chaining
 const res = text.toUpperCase().trim().substring(0, 5);
 console.log(res);
+
+const resp = {
+  stausCode: 500,
+  ok: false,
+};
+
+if (resp.ok) {
+  console.log("Everything is fine");
+}
+
+// ?Array
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// push
+number.push(10);
+
+console.log(number.includes(5));
+
+// forEach
+
+console.log(number[1] * 2);
+number.forEach((elem) => {
+  console.log(elem * 2);
+});
