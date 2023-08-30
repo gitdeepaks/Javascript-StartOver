11eabba519a4ff6cfd273b3135b6baa3a152c85c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-30 20:59:34 +0530

    Commit message

--- 16-Projests-&-Practice/03-Intermediate-stuff/fetchApi.js
+++ 16-Projests-&-Practice/03-Intermediate-stuff/fetchApi.js
@@ -2,3 +2,8 @@ fetch("https://reqres.in/api/users")
   .then((response) => response.json())
   .then((data) => console.log(data.data[0].first_name))
   .catch((error) => console.log(error));
+
+// DOM (Intermediate)
+const buttonEl = document.querySelector(".btn");
+
+buttonEl.innerHTML = "hi<span>Hello</span>";
