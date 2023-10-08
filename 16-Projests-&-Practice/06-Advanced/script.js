04ab9f4ae1731347beee63f70679e705c668d6ea
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-08 17:11:11 +0530

    Commit message

--- 16-Projests-&-Practice/06-Advanced/script.js
+++ 16-Projests-&-Practice/06-Advanced/script.js
@@ -1,34 +1,16 @@
-const number = [10, 100, 500, 1000];
+const btnEl = document.querySelector(".btn");
 
-// push
-number.push(10000);
-console.log(number); // [10, 100, 500, 1000, 10000]
+const clickHandler = () => {
+  fetch("https://reqres.in/api/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.data[3].first_name);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-//forEach
-number.forEach((num) => {
-  console.log(num + 10);
-});
-
-//map
-const newNumber = number.map((num) => {
-  return num + 10;
-});
-// console.log(newNumber); // [20, 110, 510, 1010, 10010] we get a new array
-
-// /some
-const num = number.some((num) => {
-  return num > 100000;
-});
-//  console.log(num); // yes
-
-// find
-const res = number.find((num) => {
-  return num > 8000;
-});
-// console.log(res); // 10000
-// filter()
-const newNum = number.filter((num) => {
-  return num > 400;
-});
-
-// console.log(newNum); // [500, 1000, 10000]
+btnEl.addEventListener("click", clickHandler);
