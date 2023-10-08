48de643c9d96fbbdf7803054d159b5233aaa1a14
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-08 18:01:43 +0530

    Commit message

--- 16-Projests-&-Practice/06-Advanced/script.js
+++ 16-Projests-&-Practice/06-Advanced/script.js
@@ -1,16 +1,30 @@
 const btnEl = document.querySelector(".btn");
 
-const clickHandler = () => {
-  fetch("https://reqres.in/api/users")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data.data[3].first_name);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const clickHandler = async () => {
+  // fetch("https://reqres.in/api/users")
+  //   .then((res) => {
+  //     if (!res.ok) {
+  //       console.log("problem");
+  //       return;
+  //     }
+  //     return res.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data.data[3].first_name);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  // async await
+
+  try {
+    const res = await fetch("https://reqres.in/api/users");
+    const data = await res.json();
+    console.log(data.data[3].first_name);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 btnEl.addEventListener("click", clickHandler);
