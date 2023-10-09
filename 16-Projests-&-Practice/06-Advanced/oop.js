a56a6c1429de398d7702442f6314a29966461b0c
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-09 16:14:04 +0530

    Commit message

---
+++ 16-Projests-&-Practice/06-Advanced/oop.js
@@ -0,0 +1,11 @@
+//OOP in JS
+// Object literal
+const apt1 = {
+  sqMeter: 100,
+  numOfBedRooms: 4,
+  isBig: function () {
+    return sqMeter > 100 ? true : false;
+  },
+};
+
+console.log(apt1);
