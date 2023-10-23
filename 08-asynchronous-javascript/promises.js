3d4ec408c7e783e7897a36c2ca88eeebde8f7fac
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-23 12:09:52 +0530

    Commit message

--- 08-asynchronous-javascript/promises.js
+++ 08-asynchronous-javascript/promises.js
@@ -1,9 +1,34 @@
 // promises are used to handle asynchronous operations in JavaScript
 
-const cart = ["shos", "television", "laptop", "phone", "watch"];
+// const cart = [
+//   "shos",
+//   "television",
+//   "laptop",
+//   "phone",
+//   "watch",
+//   "kurta",
+//   "pants",
+//   "shirt",
+// ];
 
-createOrder(cart, function () {
-  proceedtoPayment(orderId);
-}); //inverson of control
+// createOrder(cart, function () {
+//   proceedtoPayment(orderId);
+// }); //inverson of control
 
-const promise = createOrder(cart);
+// const promise = createOrder(cart);
+
+// // {data: {orderId: 1234}, error: null}
+// promise.then(function (orderId) {
+//   proceedtoPayment(orderId);
+// });
+
+// fetch("https://api.github.com/users/techsithgit");
+
+const user = fetch("https://api.github.com/users/gitdeepaks");
+
+console.log(user);
+user.then(function (response) {
+  console.log(response);
+});
+
+//pending -> fulfilled or rejected  are the states of a promise
