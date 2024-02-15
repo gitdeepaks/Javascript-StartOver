2241d1af25d33d755f11773e0aec59d5c50a2414
gitdeepaks <deepaksankhyan92@outlook.com>
2024-02-15 10:29:53 +0530

    Commit message

---
+++ 00-fundamentals/test.js
@@ -0,0 +1,10 @@
+//Initialize variable "x" and assign it "Hello World"
+
+let x = "Hello World";
+
+//Print the variable in console
+console.log(x);
+//Re-assign "Variables are fun" to "x"
+x = "Variables are fun";
+//Print the variable ini console
+console.log(x);
