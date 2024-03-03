80d039cb11f59c9cb579010d3eae1a767c9301c3
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-03 20:21:36 +0530

    Commit message

--- 00-fundamentals/CodingChallange01.js
+++ 00-fundamentals/CodingChallange01.js
@@ -1,12 +1,14 @@
-const massMark = 78
-const heightMark = 1.69
+const massMark = 78;
+const heightMark = 1.69;
 
-const massJohn = 92
-const heightJohn = 1.95
+const massJohn = 92;
+const heightJohn = 1.95;
 
-const BMImark = massMark / heightMark ** 2
-const bmiJohn = massJohn / (heightJohn * heightJohn)
-const markHeier = BMImark > bmiJohn
+const BMImark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+const markHeier = BMImark > bmiJohn;
 console.log(BMImark, bmiJohn);
 
 console.log(markHeier);
+
+console.log(`Is Mark's BMI higher than John's? ${markHeier}`);
