561975583d5f4a39a1b3169bdca34f6955e61bf6
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-25 18:15:47 +0530

    Commit message

---
+++ 19-Objects/ObjApi.js
@@ -0,0 +1,19 @@
+const course = {
+  courseName: "JS in Hindi",
+  price: "1000",
+  CourseInstructor: "Deepak",
+};
+
+// course.CourseInstructor;
+
+const { CourseInstructor: instructor } = course;
+
+console.log(instructor);
+
+// {
+//     "name":'deepak',
+//     "courseName":'React in Hindi',
+//     "price":'free'
+// }
+
+[{}, {}, {}];
