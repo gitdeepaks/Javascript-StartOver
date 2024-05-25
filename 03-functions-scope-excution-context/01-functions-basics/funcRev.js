8f2dd6b967886e7a56e0f1893691986780ebadfd
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-25 20:51:29 +0530

    Commit message

--- 03-functions-scope-excution-context/01-functions-basics/funcRev.js
+++ 03-functions-scope-excution-context/01-functions-basics/funcRev.js
@@ -33,6 +33,37 @@ function logedinUserMessage(username = "Sam") {
 }
 
 // const newRes = logedinUserMessage("Deepak");
-const newRes = logedinUserMessage("Deepak");
+// const newRes = logedinUserMessage("Deepak");
 
-console.log(newRes);
+// console.log(newRes);
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 7000, 10000, 20000, 700000));
+
+const user = {
+  username: "deepak",
+  price: 991,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  username: "Deepak",
+  price: 3999,
+});
+
+const myNewArr = [200, 400, 600, 999];
+
+function returnSecondValue(getArr) {
+  return getArr[1];
+}
+
+console.log(returnSecondValue([200, 500, 1000]));
