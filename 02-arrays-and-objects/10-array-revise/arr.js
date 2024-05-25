c49b66b72d876b37ff0785567562181fe409b90f
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-25 12:14:43 +0530

    Commit message

--- 02-arrays-and-objects/10-array-revise/arr.js
+++ 02-arrays-and-objects/10-array-revise/arr.js
@@ -1,1 +1,35 @@
-const myArr = [];
+const myArr = [1, 2, 3, 4, 5, 6, 7];
+
+const myHeros = ["JuniorG", "Shaktimaan"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// console.log(myArr[1]);
+// console.log(myArr2);
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(10));
+// console.log(myArr.indexOf(4));
+
+const newArr = myArr.join();
+console.log(myArr);
+// console.log(typeof newArr);
+
+// slice splice
+
+console.log("A", myArr);
+
+const nArr1 = myArr.slice(1, 3);
+
+console.log(nArr1);
+console.log("B", myArr);
+
+const nArr2 = myArr.splice(1, 3);
+console.log("C", myArr);
+console.log(nArr2);
