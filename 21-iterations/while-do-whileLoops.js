02851d3b1cce4e0c6c08dd1545555550e6933187
gitdeepaks <deepaksankhyan92@outlook.com>
2024-05-26 19:24:31 +0530

    Commit message

--- 21-iterations/while-do-whileLoops.js
+++ 21-iterations/while-do-whileLoops.js
@@ -1,6 +1,20 @@
 let i = 0;
 
-while (i <= 10) {
-  console.log(`Value of index ${i}`);
-  i = i + 2;
-}
+// while (i <= 10) {
+//   console.log(`Value of index ${i}`);
+//   i = i + 2;
+// }
+
+let myArr = ["supir", "Ironman", "batman"];
+
+// let arr = 0;
+// while (arr < myArr.length) {
+//   console.log(`${myArr[arr]}`);
+//   arr++;
+// }
+
+let score = 11;
+do {
+  console.log(`score ${score}`);
+  score++;
+} while (score <= 10);
