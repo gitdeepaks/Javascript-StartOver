871e12d3bd6aaee2fb958da0ce9753f409f21dcc
gitdeepaks <deepaksankhyan92@outlook.com>
2024-08-20 14:22:47 +0530

    Commit message

--- 27-lexical-scoping-and-closures/closure.js
+++ 27-lexical-scoping-and-closures/closure.js
@@ -5,12 +5,38 @@
 //   document.body.style.backgroundColor = `green`;
 // };
 
-function clickHandler(color) {
-  //   document.body.style.backgroundColor = `${color}`;
-  return function () {
-    document.body.style.backgroundColor = `${color}`;
-  };
-}
+// function clickHandler(color) {
+//   //   document.body.style.backgroundColor = `${color}`;
+//   return function () {
+//     document.body.style.backgroundColor = `${color}`;
+//   };
+// }
 
-document.getElementById("orange").onclick = clickHandler("orange");
-document.getElementById("green").onclick = clickHandler("green");
+// document.getElementById("orange").onclick = clickHandler("orange");
+// document.getElementById("green").onclick = clickHandler("green");
+const privateCounter = () => {
+  let count = 0;
+
+  return {
+    increment: (val = 1) => {
+      count += val;
+    },
+    getVal: () => {
+      return count;
+    },
+  };
+};
+
+const counter = privateCounter();
+console.dir(counter.getVal());
+counter.increment();
+console.dir(counter.getVal());
+
+const privateSecret = () => {
+  const secret = "foo";
+
+  return () => secret;
+};
+
+const getSecret = privateSecret();
+console.log(getSecret());
