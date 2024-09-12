c31433e86fd2d789e960111d1b7764b914b151aa
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-12 16:31:14 +0530

    Commit message

--- 29-Interview/objectsInJs.js
+++ 29-Interview/objectsInJs.js
@@ -1,34 +1,44 @@
-// let obj = {
-//   name: "Deepak Sankhyan",
-//   age: 25,
+// // // let obj = {
+// // //   name: "Deepak Sankhyan",
+// // //   age: 25,
+// // // };
+// // // console.log(num.__proto__);
+
+// // // let num = 10;
+// // // let name = "Deepak the procoder";
+
+// // // prototypal ingeritance
+
+// // function Animal(name) {
+// //   this.name = name;
+// // }
+
+// // aanimal.prototype.sayName = function () {
+// //   console.log(`My name is ${this.name}`);
+// // };
+
+// // let aanimal1 = new Animal("Tiger");
+
+// // function Dog(name, breed) {
+// //   Animal.call(this, name);
+// //   this.breed = breed;
+// // }
+
+// // Dog.prototype = Object.create(Animal.prototype);
+
+// // Dog.prototype.constructor = Dog;
+// // Dog.prototype.barks = function () {
+// //   console.log("woof");
+// // };
+
+// // var dog1 = new Dog("Max", "Labrador");
+
+// function Vehicle() {}
+
+// Vehicle.prototype.drive = () => {
+//   console.log("Drive");
 // };
-// console.log(num.__proto__);
 
-// let num = 10;
-// let name = "Deepak the procoder";
+// function Car() {}
 
-// prototypal ingeritance
-
-function Animal(name) {
-  this.name = name;
-}
-
-aanimal.prototype.sayName = function () {
-  console.log(`My name is ${this.name}`);
-};
-
-let aanimal1 = new Animal("Tiger");
-
-function Dog(name, breed) {
-  Animal.call(this, name);
-  this.breed = breed;
-}
-
-Dog.prototype = Object.create(Animal.prototype);
-
-Dog.prototype.constructor = Dog;
-Dog.prototype.barks = function () {
-  console.log("woof");
-};
-
-var dog1 = new Dog("Max", "Labrador");
+// Car.prototype = Object.create;
