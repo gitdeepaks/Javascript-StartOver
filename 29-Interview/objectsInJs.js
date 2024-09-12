80963529426d60697164b90961c589fd2c6ef287
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-12 16:27:03 +0530

    Commit message

---
+++ 29-Interview/objectsInJs.js
@@ -0,0 +1,34 @@
+// let obj = {
+//   name: "Deepak Sankhyan",
+//   age: 25,
+// };
+// console.log(num.__proto__);
+
+// let num = 10;
+// let name = "Deepak the procoder";
+
+// prototypal ingeritance
+
+function Animal(name) {
+  this.name = name;
+}
+
+aanimal.prototype.sayName = function () {
+  console.log(`My name is ${this.name}`);
+};
+
+let aanimal1 = new Animal("Tiger");
+
+function Dog(name, breed) {
+  Animal.call(this, name);
+  this.breed = breed;
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+
+Dog.prototype.constructor = Dog;
+Dog.prototype.barks = function () {
+  console.log("woof");
+};
+
+var dog1 = new Dog("Max", "Labrador");
