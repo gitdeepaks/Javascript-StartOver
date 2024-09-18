901668a1ad456da10723f97bcf508658338a1e34
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-18 16:05:14 +0530

    Commit message

--- 29-Interview/AddElemInArr.js
+++ 29-Interview/AddElemInArr.js
@@ -8,3 +8,35 @@ const append = function (arr, el) {
 
 console.log(append(numbers, 3));
 console.log(numbers);
+
+function sayHello() {
+  console.log("hello");
+}
+
+//what is closure ?
+// closure is a function that remembers and has access to variables in the local scope in which it was created.
+
+// what is lexical scoping ?
+// lexical scoping means that the scope of a variable is limited to the block in which it was declared.
+
+// please explain "this" in Javascript ?
+// "this" keyword is used to refer to the object that is executing the current function.
+
+// give a exaple in code of "this" in Javascript ?
+// "this" keyword is used to refer to the object that is executing the current function.
+
+class User {
+  /**
+   * Creates a new User object
+   * @param {string} name - the name of the user
+   */
+  constructor(name) {
+    this.name = name;
+  }
+  /**
+   * Prints the name of the user
+   */
+  printName() {
+    console.log(this.name);
+  }
+}
