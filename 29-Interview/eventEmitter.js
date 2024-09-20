75f5a2f206e9a4d0441160adc1959ca48e74b054
gitdeepaks <deepaksankhyan92@outlook.com>
2024-09-20 17:02:54 +0530

    Commit message

---
+++ 29-Interview/eventEmitter.js
@@ -0,0 +1,45 @@
+class EeventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, listner) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listner);
+  }
+
+  off(event, listnerToRemove) {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter(
+      (listner) => listner !== listnerToRemove
+    );
+  }
+  once(event, listner) {
+    const wrapper = (...args) => {
+      listner(...args);
+      this.off(event, wrapper);
+    };
+    this.on(event, wrapper);
+  }
+  emit(event, ...args) {
+    if (!this.events[event]) return;
+    this.events[event].forEach((listner) => listner.apply(this, args));
+  }
+}
+//Usage
+const emitter = new EeventEmitter();
+
+function greet(name) {
+  console.log(`Hello, ${name}`);
+}
+
+emitter.on("greet", greet);
+emitter.emit("greet", "Brad");
+emitter.off("greet", greet);
+emitter.emit("greet", "Brad");
+
+emitter.once("welcome", (name) => console.log(`Welcome ${name}`));
+emitter.emit("welcome", "Charle");
+emitter.emit("welcome", "Antonio");
