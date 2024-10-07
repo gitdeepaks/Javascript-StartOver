c4aa4beee5e0ef6d3405d07a0414121d3ff87b3e
gitdeepaks <deepaksankhyan92@outlook.com>
2024-10-07 08:55:36 +0530

    Commit message

---
+++ 30-React-interview/store.js
@@ -0,0 +1,8 @@
+// import { create } from "zustand";
+
+// const useStore = create((set) => {
+//   count: 0, increaseCount:()=>set((state)=>({count:state.count+1})),
+//   removeCount:()=>set({count:0}),
+//   updateCount:(newCount)=>set({count:newCount})
+
+// });
